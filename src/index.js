936bb40415764a7013375d92971cf3867d34ce22
Mathvivas <mathvivas@hotmail.com>
2021-11-06 11:40:03 -0300

    Criaçao do Projeto e Listagem dos Integrantes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+// Escolha o tema que desejar
+// https://primefaces.org/primereact/showcase/#/setup
+import "primereact/resources/themes/bootstrap4-dark-purple/theme.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={createStore(reducers)}>
+        <App />,
+    </Provider>,
+    document.querySelector("#root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
